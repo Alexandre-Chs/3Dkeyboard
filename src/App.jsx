0236f3050c5fccb607ac32da229eb8fbf4987135
Alexandre <alexandrechanas0@gmail.com>
2022-10-30 12:25:09 +0100

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Spline from "@splinetool/react-spline";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+      <Spline scene="https://prod.spline.design/yxjpYnTbSMXbM2WO/scene.splinecode" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Keyboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+const Keyboard = () => {
+  return (
+    <Spline scene="https://prod.spline.design/yxjpYnTbSMXbM2WO/scene.splinecode" />
+  );
+};
+
+export default Keyboard;
